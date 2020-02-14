ebd07b30b9e3982f06b20124825d968236d0ea25
jerry-Wu112233 <yihanwu4@illinois.edu>
2020-02-14 14:47:24 -0600

    implemented traitTestCommand and some message handling behaviors

--- src/main/scala/example/ActorAugmented.scala
+++ src/main/scala/example/ActorAugmented.scala
@@ -7,8 +7,13 @@ import Reference._
 object ActorAugmented {
   // message protocol
   sealed trait GCommand
-  final case class Release(releasing: Set[Reference], creations: Set[Reference])
-  final case class Share(ref: Reference)
+  final case class Release(releasing: Set[Reference], creations: Set[Reference]) extends GCommand
+  final case class Share(ref: Reference) extends GCommand
+
+  sealed trait TestCommand
+  final case class Spawn() extends TestCommand
+  final case class TestShare(to: Reference, target: Reference) extends TestCommand
+  final case class TestRelease() extends TestCommand
 
   def apply(creator: ActorRef[Any], token: Token): Behavior[Any] = {
     Behaviors.setup(context => new ActorAugmented(context, creator, token))
@@ -20,10 +25,27 @@ class ActorAugmented(context: ActorContext[Any], creator: ActorRef[Any], token: 
 
   private var refs: Set[Reference] = Set()
   private var created: Set[Reference] = Set()
-  private var owners: Set[Reference] = Set()
+  private var owners: Set[Reference] = Set(Reference(token, creator, context.self))
   private var released_owners: Set[Reference] = Set()
+  private var tokenCount: Int = 0;
+
+  def createToken(): Token = {
+    val token = Token(context.self, tokenCount)
+    tokenCount += 1
+    token
+  }
 
   override def onMessage(msg: Any): Behavior[Any] = {
-    this
+    import ActorAugmented._
+    msg match{
+      case Spawn() =>
+        val token = createToken()
+        val childRef = context.spawn(ActorAugmented(context, context.self, token), "child")
+        refs += Reference(token, context.self, childRef)
+        Behaviors.same
+      case TestShare() =>
+        val token1 = createToken()
+
+    }
   }
 }
