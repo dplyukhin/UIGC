7eb15918dac535695a710845fc4d78ef306b518e
Charles <charlie.kuch@gmail.com>
2020-02-25 14:11:15 -0600

    Changes made during Thursday meeting

--- src/main/scala/example/ActorAugmented.scala
+++ src/main/scala/example/ActorAugmented.scala
@@ -2,14 +2,20 @@ package example
 
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+import example.ActorAugmented.GCMessage
 
 case class Token(ref: ActorRef[Nothing], n: Int)
 case class GCRef[-T](x: Token, from: ActorRef[Nothing], to: ActorRef[T])
 
 object ActorAugmented {
   // message protocol
-  sealed trait InternalCommand
-  final case class Release(releasing: Set[GCRef[Nothing]], creations: Set[GCRef[Nothing]]) extends InternalCommand
+  /**
+   * The type of actor that GCMessage wraps
+   * @tparam T
+   */
+  sealed trait GCMessage[T]
+  final case class Release[T](releasing: Iterable[GCRef[T]], creations: Iterable[GCRef[T]]) extends GCMessage[T]
+  final case class App[T](payload: T) extends GCMessage[T]
   // a message with a set of references for the recipient to add to its own refs collection
 //  final case class ReceiveRefs(receivedRefs: Set[GCRef]) extends InternalCommand
 //  object ReceiveRefs {
@@ -17,33 +23,39 @@ object ActorAugmented {
 //  }
 
 
-  final case class RefReply(refs: GCRef[Nothing])
-
-  sealed trait ExternalCommand
-  final case class Spawn[T](replyTo: GCRef[Nothing], actorType: Behavior[T]) extends ExternalCommand
-  final case class Share(target: GCRef[Nothing], recipient: GCRef[Nothing]) extends ExternalCommand
-  final case class Forget(fancyRefs: Set[GCRef[Nothing]]) extends ExternalCommand
-//  final case class Release() extends ExternalCommand
-
-
   def apply(creator: ActorRef[Any], token: Token): Behavior[Any] = {
     Behaviors.setup(context => new ActorAugmented(context, creator, token))
   }
 }
 
-class ActorAugmented(context: ActorContext[Any], creator: ActorRef[Any], token: Token)
-  extends AbstractBehavior[Any](context) {
-
+class ActorAugmented[T](context: ActorContext[GCMessage[T]], creator: ActorRef[Nothing], token: Token)
+  extends AbstractBehavior[GCMessage[T]](context) {
+  import ActorAugmented._
   private var refs: Set[GCRef[Nothing]] = Set()
   private var created: Set[GCRef[Nothing]] = Set()
   private var owners: Set[GCRef[Nothing]] = Set(GCRef(token, creator, context.self))
   private var released_owners: Set[GCRef[Nothing]] = Set()
   private var tokenCount: Int = 0;
 
+  /**
+   *
+   * @return
+   */
   private def createToken(): Token = {
     val token = Token(context.self, tokenCount)
     tokenCount += 1
     token
+  }
+
+  /**
+   *
+   * @param behavior
+   * @tparam T
+   * @return
+   */
+  private def wrap[T](behavior: Behavior[T]): Behavior[GCMessage[T]] = {
+    // put something to extend context here??
+    // context => Behavior[T]
   }
 
   private def spawn[U](behavior_callback: (ActorRef[Nothing], Token) => Behavior[U]): GCRef[Nothing] = {
@@ -60,51 +72,57 @@ class ActorAugmented(context: ActorContext[Any], creator: ActorRef[Any], token: 
    * @param recipient The actor the reference is being sent to.
    * @return A [[GCRef]] pointing from the recipient to the target
    */
-  def createRef[T](target: GCRef[T], recipient: GCRef[Nothing]): GCRef[T] = {
+  def createRef[U](target: GCRef[U], recipient: GCRef[Nothing]): GCRef[U] = {
     val token = createToken()
     val sharedRef = GCRef(token, recipient.to, target.to)
     created += sharedRef
     sharedRef
   }
 
-  def forget(releasing: Set[GCRef[Nothing]]): Unit = {
+  /**
+   * assuming everything in the releasing set points to the same actor
+   * @param releasing
+   * @tparam T
+   */
+  def forget[T](releasing: Seq[GCRef[ActorAugmented.Release[T]]]): Unit = {
     import ActorAugmented._
     // for every reference that is being released
-    releasing.foreach((gcRef => {
-      val toForget = gcRef.to // get the target being released
-      val creations: Set[GCRef[Nothing]] = created.filter {
-        createdRef => createdRef.to == toForget
-      } // get the refs in the created set that point to the target
-      created --= creations // remove those refs from created
-      toForget ! Release(releasing, creations) // send the message to the target that we are forgetting it
-    }))
+    val toForget = releasing.head.to
+    val creations = created.filter {
+      createdRef =>
+        createdRef.to == toForget
+    }
+    created --= creations // remove those refs from created
+    toForget ! Release(releasing, creations) // send the message to the target that we are forgetting it
   }
 
-  override def onMessage(msg: Any): Behavior[Any] = {
+  override def onMessage(msg: GCMessage[T]): Behavior[GCMessage[T]] = {
     import ActorAugmented._
-    msg match{
-//      case Spawn[T](replyTo: GCRef[T], actorType: Behavior[T]) =>
-//        replyTo ! RefReply(spawn(actorType))
-//        Behaviors.same
-//      case ReceiveRefs(receivedRefs) =>
-//          refs ++= receivedRefs
-//        Behaviors.same
+    msg match {
       case Release(releasing, creations) =>
-        // for each ref in releasing
-        owners --= releasing intersect owners // if the ref is in owners, remove it
-        released_owners ++= releasing diff owners // else, add to released_owners
-        // for each ref in creations
-        released_owners --= creations intersect released_owners // if the ref is in releaseD_owners, remove it
-        owners ++= creations diff owners // else, add to owners
+        releasing.foreach(ref => {
+          if (owners.contains(ref)) {
+            owners -= ref
+          }
+          else {
+            released_owners += ref
+          }
+        })
+        creations.foreach(ref => {
+          if (released_owners.contains(ref)) {
+            released_owners -= ref
+          }
+          else {
+            owners += ref
+          }
+        })
         if (owners.isEmpty && released_owners.isEmpty) {
+          // TODO release any refs held by this actor
           Behaviors.stopped
         }
         else {
           Behaviors.same
         }
-      case Forget(releasing) =>
-        forget(releasing)
-        Behaviors.same
     }
   }
 }
