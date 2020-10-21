dd6933bd48621a58ea8756177e56b2d713923174
Dan Plyukhin <dplyukhin@gmail.com>
2020-10-21 12:02:54 -0400

    Implemented sigmoid function for snapshot delay

--- src/main/scala/gc/AbstractBehavior.scala
+++ src/main/scala/gc/AbstractBehavior.scala
@@ -3,7 +3,7 @@ package gc
 import akka.actor.typed.scaladsl.{AbstractBehavior => AkkaAbstractBehavior}
 import akka.actor.typed.{Behavior => AkkaBehavior}
 
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.duration.{DurationDouble, DurationInt, FiniteDuration}
 
 case object SnapshotTimer
 
@@ -36,7 +36,19 @@ abstract class AbstractBehavior[T <: Message](context: ActorContext[T])
    * @param snapshotCount The number of snapshots this actor has taken so far.
    * @return The mimimum amount of time this actor should wait before taking another snapshot.
    */
-  def snapshotDelay(snapshotCount: Int): FiniteDuration = 1.second
+  def snapshotDelay(snapshotCount: Int): FiniteDuration = {
+    // The maximum delay
+    val maxDuration = 2.0
+    // How quickly an actor reaches the maximum delay
+    val steepness = 1.5
+    // This determines the initial delay and when the maximum delay is reached;
+    // a large negative value means a small initial delay
+    val shift = -3
+
+    // Use a logistic function to gradually increase the snapshot delay,
+    // up to a certain maximum
+    (maxDuration / (1.0 + scala.math.pow(2, steepness * (snapshotCount - shift)))).seconds
+  }
 
   def onMessage(msg: T): Behavior[T]
 
@@ -60,7 +72,7 @@ abstract class AbstractBehavior[T <: Message](context: ActorContext[T])
         context.handleSelfCheck()
         context.tryTerminate()
       case TakeSnapshot =>
-        println(s"${context.self} Taking a snapshot: ${context.snapshot}")
+        // println(s"${context.self} Taking a snapshot: ${context.snapshot}")
         snapshotCount += 1
         // do not restart the timer yet - wait until we have received another message,
         // and therefore possibly updated the snapshot's contents
