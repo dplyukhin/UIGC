7475e066b9227f224767a5f858ddc6a227025a9f
Dan P <dplyukhin@gmail.com>
2023-06-07 11:45:57 -0500

    WIP: Updated the middleware API

--- lib/src/main/scala/edu/illinois/osl/akka/gc/protocols/monotone/Refob.scala
+++ lib/src/main/scala/edu/illinois/osl/akka/gc/protocols/monotone/Refob.scala
@@ -2,21 +2,7 @@ package edu.illinois.osl.akka.gc.protocols.monotone
 
 import edu.illinois.osl.akka.gc.interfaces._
 
-/**
- * An opaque and globally unique token.
- */
-class Token(val creator: Name, val seqnum: Int, val targetShadow: Shadow) extends Pretty {
-  override def equals(obj: Any): Boolean = {
-    obj match {
-      case other: Token => this.creator == other.creator && this.seqnum == other.seqnum
-    }
-  }
-  def pretty: String = s"Token#${Math.floorMod(this.hashCode(), 1000)}"
-}
-
 case class Refob[-T](
-  token: Option[Token],
-  owner: Option[RefLike[GCMessage[Nothing]]],
   target: RefLike[GCMessage[T]],
 ) extends RefobLike[T] {
 
@@ -28,8 +14,6 @@ case class Refob[-T](
     hasChangedThisPeriod = false
   }
 
-  override def pretty: String = {
-    f"<Refob#${Math.floorMod(token.hashCode(), 1000)}: ${owner.get.pretty} -> ${target.pretty}>"
-  }
+  override def pretty: String = target.pretty
 
 }
