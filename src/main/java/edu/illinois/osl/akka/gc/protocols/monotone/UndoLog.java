39c0cb90eba9186885ab0fdceb7c295e11569c3b
Dan P <dplyukhin@gmail.com>
2023-07-28 11:43:58 -0500

    Implemented merging ingress entries and delta into undo logs

--- src/main/java/edu/illinois/osl/akka/gc/protocols/monotone/UndoLog.java
+++ src/main/java/edu/illinois/osl/akka/gc/protocols/monotone/UndoLog.java
@@ -5,6 +5,7 @@ import akka.actor.Address;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The undo log indicates how many messages should be marked "un-sent" and how many references
@@ -14,11 +15,6 @@ import java.util.HashMap;
 public class UndoLog {
     /** The address of the node whose effects we are undoing. */
     Address nodeAddress;
-    /** The highest sequence number delta graph that has been merged into the log so far. */
-    int deltaSeqnum;
-    /** For each node adjacent to [[nodeAddress]], this field indicates the highest sequence number
-     * of an ingress actor at that node. */
-    HashMap<Address, Integer> ingressSeqnum;
     HashMap<ActorRef, Field> admitted;
 
     public static class Field implements Serializable {
@@ -34,6 +30,67 @@ public class UndoLog {
     }
 
     public void mergeDeltaGraph(DeltaGraph delta) {
+        // This will act as a hashmap, mapping compressed IDs to actorRefs.
+        akka.actor.typed.ActorRef<?>[] refs = new akka.actor.typed.ActorRef<?>[delta.currentSize];
+        for (Map.Entry<akka.actor.typed.ActorRef<?>, Short> entry : delta.compressionTable.entrySet()) {
+            refs[entry.getValue()] = entry.getKey();
+        }
 
+        for (short i = 0; i < delta.currentSize; i++) {
+            DeltaShadow deltaShadow = delta.shadows[i];
+            if (deltaShadow.interned)
+                // We only care about messages sent and references created by this node
+                // *for actors on other nodes*.
+                continue;
+
+            ActorRef thisActor = refs[i].classicRef();
+            Field field = admitted.get(thisActor);
+            if (field == null) {
+                field = new Field();
+            }
+
+            // Undo any messages this node claims to have sent to the recipient actor
+            field.messageCount -= deltaShadow.recvCount;
+
+            // Undo any of the references this node claims to have created for the recipient
+            for (Map.Entry<Short, Integer> entry : deltaShadow.outgoing.entrySet()) {
+                ActorRef targetActor = refs[entry.getKey()].classicRef();
+                int count = entry.getValue();
+                updateOutgoing(field.createdRefs, targetActor, -count);
+            }
+        }
+    }
+
+    public void mergeIngressEntry(IngressEntry entry) {
+        for (Map.Entry<ActorRef, IngressEntry.Field> pair : entry.admitted.entrySet()) {
+            ActorRef actor = pair.getKey();
+            IngressEntry.Field entryField = pair.getValue();
+
+            Field field = this.admitted.get(actor);
+            if (field == null) {
+                field = new Field();
+            }
+
+            // Add back the number of messages that actually got admitted to the node.
+            field.messageCount += entryField.messageCount;
+
+            for (Map.Entry<ActorRef, Integer> refCount : entryField.createdRefs.entrySet()) {
+                ActorRef targetActor = refCount.getKey();
+                int count = refCount.getValue();
+                // Add back the references that actually got delivered to the node.
+                updateOutgoing(field.createdRefs, targetActor, count);
+            }
+        }
+    }
+
+    public void updateOutgoing(Map<ActorRef, Integer> outgoing, ActorRef target, int delta) {
+        int count = outgoing.getOrDefault(target, 0);
+        if (count + delta == 0) {
+            // Instead of writing zero, we delete the count.
+            outgoing.remove(target);
+        }
+        else {
+            outgoing.put(target, count + delta);
+        }
     }
 }
