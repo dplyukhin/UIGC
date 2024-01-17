ac386d4e4a80cd1c10f7760ef69c62fa135d07bd
Nikolai Levshakov <nikolai_levshakov@epam.com>
2024-01-17 20:23:00 +0100

    Updated README file

--- scripts/build-linux-mac.sh
+++ scripts/build-linux-mac.sh
@@ -4,7 +4,7 @@ git clone https://github.com/dplyukhin/akka.git
 cd akka
 sbt publishLocal | tee build.info
 AKKA_VERSION=$(grep -o '[a-zA-Z0-9+-.]*-SNAPSHOT' build.info | head -1)
-echo "AKKA_VERSION=$AKKA_VERSION"
+echo "[build_script] AKKA_VERSION=$AKKA_VERSION"
 
 cd ../..
 sed -i "s/val akkaVersion =.*/val akkaVersion = \"$AKKA_VERSION\"/g" build.sbt
@@ -12,4 +12,4 @@ sed -i "s/val akkaVersion =.*/val akkaVersion = \"$AKKA_VERSION\"/g" build.sbt
 sbt publishLocal
 
 rm -rf scripts/akka
-echo "DONE"
+echo "[build_script] DONE"
