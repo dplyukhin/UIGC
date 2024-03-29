183b4de0edc7a4206994ca29bdaa6d56db0e4a94
Dan Plyukhin <dplyukhin@gmail.com>
2024-03-29 15:21:16 -0500

    Added a disabled event to track stats about sending entries

---
+++ src/main/java/edu/illinois/osl/uigc/engines/crgc/jfr/EntrySendEvent.java
@@ -0,0 +1,15 @@
+package edu.illinois.osl.uigc.engines.crgc.jfr;
+
+import jdk.jfr.*;
+
+@Label("CRGC Entry Sent")
+@Category("UIGC")
+@Description("An actor sending an update about its local GC state to the local garbage collector.")
+@StackTrace(false)
+@Enabled(false)
+public class EntrySendEvent extends Event {
+
+    @Label("Was memory allocated?")
+    public boolean allocatedMemory;
+
+}
